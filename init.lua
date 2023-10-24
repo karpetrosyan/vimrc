8e0012e90f819489ec10eebf0b7e56d4f22c43ef
karosis88 <kar.petrosyanpy@gmail.com>
2023-10-24 14:45:12 +0300

    Remove plugins

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
-require("core.keymaps")
-require("core.plugins")
-require("core.plugin_config.init")
+require("plugins")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.cmd [[ set relativenumber ]] 
+vim.cmd [[ colorscheme gruvbox ]]
+
+vim.keymap.set('n', '<c-t>', function() vim.cmd [[ e . ]] end)
+
+vim.keymap.set('n', '<c-h>', function() vim.cmd [[ bprevious ]] end)
+vim.keymap.set('n', '<c-l>', function() vim.cmd [[ bnext ]] end)
+
